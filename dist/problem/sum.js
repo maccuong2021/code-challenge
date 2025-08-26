7cd14dd708db8f254e3dce7d17a778c9004175a0
Cuong Truong <maccuong@gmail.com>
2025-08-26 14:19:20 +0700

    update

---
+++ dist/problem/sum.js
@@ -0,0 +1,26 @@
+"use strict";
+function sum_to_n(n) {
+    return (n * (n + 1)) / 2;
+}
+function sum_to_n_2(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+function sum_to_n_3(n) {
+    if (n <= 1)
+        return n;
+    return n + sum_to_n_3(n - 1);
+}
+// Get input from command line
+const input = process.argv[2];
+const n = parseInt(input);
+if (isNaN(n)) {
+    console.error("Please provide a valid integer.");
+    process.exit(1);
+}
+console.log(`Sum from 1 to ${n} is: ${sum_to_n(n)}`);
+console.log(`Sum from 1 to ${n} is: ${sum_to_n_2(n)}`);
+console.log(`Sum from 1 to ${n} is: ${sum_to_n_3(n)}`);
